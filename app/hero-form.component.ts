ba1ce233202c2d9d646840346c0482d107e65079
anandphadnis <phadnisanandhyd@gmail.com>
2017-04-05 07:31:11 +0530

    new

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -13,31 +13,110 @@ import 'rxjs/add/operator/map';
 export class HeroFormComponent {
   data;
 
-  getAllData()
+  getAllArticlesData()
   {
-	
-        this.http.get('http://localhost:3000/app/people.json')
-                .subscribe(res => this.data = res.json());
+
+         this.http.get('http://localhost:8888/drupal-8.2.7/articles')
+                .subscribe(res => this.data = res.json()); 
+          /*this.http.get('http://localhost:3000/app/people.json')
+                .subscribe(res => this.data = res.json()); */
+  }
+
+  getSingleUser()
+  {
+                       
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+         this.http.get('http://localhost:8888/drupal-8.2.7/user/1?_format=json', {
+          headers: headers
+        })
+        .subscribe(res => this.data =  [res.json()]);
+
 
   }
 
+  getSingleNode()
+  {
+
+        let headers = new Headers();
+         this.http.get('http://localhost:8888/drupal-8.2.7/node/7?_format=json', {
+          headers: headers
+        })
+        .subscribe(res => this.data =  [res.json()]);
+
+
+  }
   createData()
   {
-	
+         let headers = new Headers();
+         headers.append('Content-Type', "application/json; charset=utf8"); 
+         this.createAuthorizationHeader(headers);
+         let body;
+           body={
+            "type":[{"target_id":"article"}],
+            "title":[{"value":"Hello Anand"}],
+            "body":[{"value":"How are you?"}]
+          };
+          this.http.post (
+          'http://localhost:8888/drupal-8.2.7/entity/node/', JSON.stringify(body), {
+            headers: headers
+          }).subscribe(); 
+
+  }
+  
+  updateData()
+  {
+
+         let headers = new Headers();
+         headers.append('Content-Type', "application/json; charset=utf8"); 
+         this.createAuthorizationHeader(headers);
+         let body;
+           body={
+            "title":[{"value":"Hello Anand"}]
+          };
+          this.http.patch(
+          'http://localhost:8888/drupal-8.2.7/entity/node/10', JSON.stringify(body), {
+            headers: headers
+          }).subscribe(); 
+
+
   }
 
- deleteData()
+ deleteData(id: number)
  {
+     
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+   return this.http.delete('http://localhost:8888/drupal-8.2.7/node'+ id, {
+        headers: headers,
+   }).subscribe(); 
+	
+ }
+
+ editData(id: number)
+ {
+
+	   this.http.get('http://localhost:8888/drupal-8.2.7/articles'+ id)
+                .subscribe(res => this.data = res.json()); 
 
  }
 
- updateData()
- {
+ createAuthorizationHeader(headers: Headers) {
+    headers.append('Authorization', 'Basic ' +
+      btoa('admin:admin')); 
+  }
+  
 
- }
+
  constructor(private http:Http) {
 
-    this.getAllData();
+     this.getAllArticlesData();
+
+     //this.getSingleUser();
+
+    // this.getSingleNode();
+
+     //this.createData();
   }
 
 
